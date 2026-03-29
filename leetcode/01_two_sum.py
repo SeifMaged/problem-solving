c436e50aae07b7f48fb6fc3e98015aef99d9fb36
Seif Maged <62437615+SeifMaged@users.noreply.github.com>
2026-03-29 13:43:22 +0200

    Added solution for Leetcode 1- Two Sum

---
+++ leetcode/01_two_sum.py
@@ -0,0 +1,15 @@
+# Hashmap
+# O(n) Time
+# O(n) Space
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i in range(len(nums)):
+            needed = target - nums[i]
+            if needed in seen:
+                return [seen[needed], i]
+            seen[nums[i]] = i
+        
+        return -1
