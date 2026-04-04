321ce9a56a7f16a657fdfbd85fccadf308b53e62
Seif <seifmagedmohamed@gmail.com>
2026-04-04 15:52:16 +0200

    Added solution to Leetcode 796 - Rotate String

---
+++ leetcode/0796_rotate_string.py
@@ -0,0 +1,6 @@
+# O(N^2) Time
+# O(N) Space
+# Interview Microsoft Question 2
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and goal in s+s
