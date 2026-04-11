ed3f57e191a3a2d0eec248a3c21016b6e31a9846
Seif <seifmagedmohamed@gmail.com>
2026-04-11 16:58:26 +0200

    Added solution for Leetcode 704

---
+++ leetcode/0704_binary_search.py
@@ -0,0 +1,19 @@
+# Easy
+# Binary Search
+# O(log n) Time, where n is the length of nums
+# O(1) Space
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return -1
