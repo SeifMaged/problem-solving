b1298d1cc3f1ac0608cf66494d1825a2c8f441e3
Seif <seifmagedmohamed@gmail.com>
2026-04-12 17:43:13 +0200

    Added Soltuion for Leetcode 102 - BFS

---
+++ leetcode/0102_binary_tree_level_order_traversal.py
@@ -0,0 +1,37 @@
+# Medium
+# Binary Tree - Breadth First Search
+# O(n) Time, where n is the number of nodes in the binary tree
+# O(n) Space, where n is the number of nodes in the binary tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        level = []
+        output = []
+
+        if root:
+            level.append(root)
+
+        
+        while level:
+            next_level = []
+            current_values = []
+
+            for node in level:
+                current_values.append(node.val)
+
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        
+            output.append(current_values)
+            level = next_level
+        
+        return output
---
+++ leetcode/094_binay_tree_inorder_traversal.py
@@ -0,0 +1,25 @@
+# Easy
+# Binary Tree - Depth First Search
+# O(n) Time, where n is the number of nodes in the binary tree
+# O(h) Space, where h is the height of the binary tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        output = []
+
+        def helper(root):
+            if root:
+                helper(root.left)
+                output.append(root.val)
+                helper(root.right)
+        
+        helper(root)
+            
+        return output
