a6647b1b05a0a0bf236253b768d02fdd88f41088
Seif <seifmagedmohamed@gmail.com>
2026-04-13 14:11:02 +0200

    Added Solution for binary tree bfs problems

---
+++ leetcode/0199_binary_tree_right_side_view.py
@@ -0,0 +1,35 @@
+# Medium
+# Binary Tree - Breadth First Search
+# O(n) Time, where n is the number of nodes in the binary tree
+# O(n) Space, where n is the number of nodes in the binary tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+        level = deque()
+        output = []
+        if root:
+            level.append(root)
+
+        while level:
+            
+            output.append(level[-1].val)
+
+            for i in range(len(level)):
+                if level[0].left:
+                    level.append(level[0].left)
+                if level[0].right:
+                    level.append(level[0].right)
+                level.popleft()
+            
+
+        return output
