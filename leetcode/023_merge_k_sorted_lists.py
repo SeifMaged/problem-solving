885bd1c70cdf746eadcef9ae4a5fa360b04f82ce
Seif <seifmagedmohamed@gmail.com>
2026-04-21 23:58:40 +0200

    Added Sol for LC 23

---
+++ leetcode/023_merge_k_sorted_lists.py
@@ -0,0 +1,40 @@
+# 23. Merge k Sorted Lists
+# https://leetcode.com/problems/merge-k-sorted-lists/
+# Hard
+# O(nk) time, O(1) space
+# Greedy, Linked List, Merge
+
+from typing import List, Optional
+#Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+
+        valid_lists = set()
+
+        for i in range(len(lists)):
+            if lists[i]: # Nonempty linked list
+                valid_lists.add(i)
+        
+        trav = dummy
+        
+        while valid_lists:
+            min_value, min_index = float('inf'), -1
+            for i in valid_lists:
+                if lists[i].val < min_value:
+                    min_value = lists[i].val
+                    min_index = i
+            
+            min_node = lists[min_index]
+            trav.next = min_node
+            trav = trav.next
+            
+            lists[min_index] = min_node.next
+            if lists[min_index] is None:
+                valid_lists.remove(min_index)
+        
+        return dummy.next
